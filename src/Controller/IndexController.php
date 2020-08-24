a666bbdb57ef1b5260287eb3e645c530ccd32be2
granger kenolane <kenolane28@gmail.com>
2020-08-24 18:45:45 +0200

    create __construct function and redirect the indexMethod to a template

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -1,13 +1,17 @@
 <?php
-
-
 namespace App\Controller;
+require_once __DIR__.'/../../config/bootstrap.php';
 
 
 class IndexController extends MainController
 {
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
     public function indexMethod(){
-        return $this->render('base.html.twig');
+    return $this->render('index/index.html.twig');
     }
 
 }
