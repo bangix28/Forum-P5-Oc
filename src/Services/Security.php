ed867cb7cc282d28a0e1efcc211bcb7d2d1b3aa7
granger kenolane <kenolane28@gmail.com>
2020-09-08 22:47:05 +0200

    remove html in controller and add him in template in array

--- src/Services/Security.php
+++ src/Services/Security.php
@@ -17,10 +17,10 @@ class Security extends MainController
                 $email = $this->verifiedEmail($this->request->getPost()->get('email'));
                 if (empty($email)) {
                     $user = new User();
-                    $user->setEmail(htmlspecialchars($this->request->getPost()->get('email')));
+                    $user->setEmail($this->request->getPost()->get('email'));
                     $user->setPassword(password_hash($this->request->getPost()->get('password'), PASSWORD_DEFAULT));
-                    $user->setFirstName(htmlspecialchars($this->request->getPost()->get('firstName')));
-                    $user->setLastName(htmlspecialchars($this->request->getPost()->get('lastName')));
+                    $user->setFirstName($this->request->getPost()->get('firstName'));
+                    $user->setLastName($this->request->getPost()->get('lastName'));
                     $user->setRoles(['ROLES_USER']);
                     $user->setLogo('1');
                     $randomKey = $this->generateKey();
@@ -28,20 +28,12 @@ class Security extends MainController
                     $em = $this->orm->entityManager();
                     $em->persist($user);
                     $em->flush();
+                    header('Location:index.php');
+
                 } else
                     return $message = "l'email rentré est déja utilisé";
             }
-            return $message = '
-            <div class="text-danger">
-               <ul class="list-group"> 
-               <p>Le mot de passe doit avoir :</p>
-                <li>au moins un caractère minuscule</li>
-                <li>au moins un caractère majuscule</li>
-                <li>au moins un chiffre </li> 
-                <li>au moins un signe spécial  @ # -_ $% ^ & + = § !? </li>
-                <li>et doit etre compris 8 et 20 caractères </li>
-               </ul>
-            </div>';
+            return $message = ['au moins un caractère minuscule', 'au moins un caractère majuscule', 'au moins un chiffre', 'au moins un signe spécial  @ # -_ $% ^ & + = § !?', 'et doit etre compris 8 et 20 caractères'];
         }
         return $message = 'Vous devez remplir tout les champs';
     }
@@ -58,31 +50,26 @@ class Security extends MainController
 
     public function login()
     {
+        session_unset();
         if (!empty($this->request->getPost()->get('email')) && !empty($this->request->getPost()->get('password'))) {
             $user = $this->verifiedEmail($this->request->getPost()->get('email'));
             if (!empty($user)) {
                 if (preg_match('/^(?=.*\d)(?=.*[@#\-_$%^&+=§!?])(?=.*[a-z])(?=.*[A-Z])[0-9A-Za-z@#\-_$%^&+=§!\?]{8,20}$/', $this->request->getPost()->get('password'))){
                 if (password_verify($this->request->getPost()->get('password'), $user->getPassword())) {
+                    $this->request->getSession()->stop();
                     $this->sessionLogin($user);
                     header('Location:index.php');
                 }
-                    return $message = 'Mot de passe incorrecte';
+                     $this->request->getSession()->set('message', "Mot de passe incorrecte" );
+                     header('Location:index.php');
                     }
-                return $message = '
-            <div class="text-danger">
-               <ul class="list-group"> 
-               <p>Le mot de passe doit avoir :</p>
-                <li>au moins un caractère minuscule</li>
-                <li>au moins un caractère majuscule</li>
-                <li>au moins un chiffre </li> 
-                <li>au moins un signe spécial  @ # -_ $% ^ & + = § !? </li>
-                <li>et doit etre compris 8 et 20 caractères </li>
-               </ul>
-            </div>';
+                $this->request->getSession()->set('message', ['au moins un caractère minuscule', 'au moins un caractère majuscule', 'au moins un chiffre', 'au moins un signe spécial  @ # -_ $% ^ & + = § !?', 'et doit etre compris 8 et 20 caractères'] );
             }
-            return $message = "Cette email n'existe pas !";
+            $this->request->getSession()->set('message', "Cette email n'existe pas !");
+            header('Location:index.php');
         }
-        return $message = 'Rempliser tout les champs';
+        $this->request->getSession()->set('message','Rempliser tout les champs');
+        header('Location:index.php');
     }
 
     public function verifiedEmail($email)
@@ -94,6 +81,7 @@ class Security extends MainController
 
     public function sessionLogin($user)
     {
+        
         $this->request->getSession()->set('id', $user->getId());
         $this->request->getSession()->set('roles', $user->getRoles());
         $this->request->getSession()->set('email', $user->getEmail());
