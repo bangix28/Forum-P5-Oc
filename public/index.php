695ee6f5b4580cf1feff8d0204b8b879f7f31b6f
granger kenolane <kenolane28@gmail.com>
2020-09-08 17:44:38 +0200

    delete the role session for guest user

--- public/index.php
+++ public/index.php
@@ -2,12 +2,10 @@
 use App\Router;
 use Tracy\Debugger;
 
-
 require_once '../vendor/autoload.php';
 
 if (session_status() == PHP_SESSION_NONE){
     session_start();
-    $_SESSION['roles'] = "ROLES_GUEST";
 }
 $router = new Router();
 Debugger::enable();
