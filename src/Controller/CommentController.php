dde31d12a5ffaa8afc76298311bc2026390d7801
granger kenolane <kenolane28@gmail.com>
2020-09-16 18:56:07 +0200

    add deleteMethod and create method

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -4,8 +4,22 @@
 namespace App\Controller;
 
 
+use App\Services\comment\CommentServices;
+
 class CommentController extends MainController
 {
+    private $commentServices;
+    public function __construct()
+    {
+        parent::__construct();
+        $this->commentServices = new CommentServices();
+
+    }
+
+    public function createMethod()
+    {
+        $this->commentServices->Verification();
+    }
     public function editMethod()
     {
 
@@ -17,6 +31,11 @@ class CommentController extends MainController
     }
     public function deleteMethod()
     {
-
+        $em = $this->orm->entityManager();
+        $comment = $em->find(':Comment', $this->request->getGet()->get('id'));
+        $id = $comment->getPost();
+        $em->remove($comment);
+        $em->flush();
+        header('Location:index.php?access=post!read&id='. $id);
     }
 }
