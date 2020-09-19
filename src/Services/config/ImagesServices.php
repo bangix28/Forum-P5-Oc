e09ff8853184d50f49b01848a360f5cd297313b2
granger kenolane <kenolane28@gmail.com>
2020-09-19 15:36:47 +0200

    Create imageServices class

---
+++ src/Services/config/ImagesServices.php
@@ -0,0 +1,43 @@
+<?php
+
+
+namespace App\Services\config;
+
+
+use App\Controller\MainController;
+
+class ImagesServices extends MainController
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function uploadImage($a, $em)
+    {
+        $files = $this->request->getFiles()->get('form');
+        if ($files['size'] <= 1000000) {
+            $infosFiles = pathinfo($files['name']);
+            $uploadExtension = $infosFiles['extension'];
+            $authorizedExtension = array('jpg', 'jpeg', 'gif', 'png');
+            if (in_array($uploadExtension, $authorizedExtension)) {
+                $name = md5($files['name']).'.'. $infosFiles['extension'];
+                move_uploaded_file($files['tmp_name'], 'assets/img/upload/' . basename($name));
+                if ($a === 'user')
+                {
+                    $user = $em->find(':User', $this->request->getSession()->get('id'));
+                    $user->setImage($name);
+                    $em->persist($user);
+                    $em->flush();
+                }elseif ($a === 'post'){
+                    return $name;
+                }
+            }else{
+                return $message = 'Extension invalide';
+            }
+        }else{
+            return $message = 'Le fichier doit faire moins de 10 Mo';
+        }
+
+        }
+}
