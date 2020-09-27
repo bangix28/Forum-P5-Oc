04bad60c551ee3859ded28d2767f8d9108344898
granger kenolane <kenolane28@gmail.com>
2020-09-27 22:05:10 +0200

    ad em variable and passwordverify

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -11,15 +11,19 @@ class UserController extends MainController
 
     private $security;
 
+    private $em;
+
     public function __construct()
     {
         parent::__construct();
         $this->userEdit = new UserEdit();
         $this->security = new Security();
+        $this->em = $this->orm->entityManager();
     }
 
     public function editMethod()
     {
+        $user = $this->request->getSession()->get('user');
         $message = null;
         $submit = $this->request->getPost()->get('submit');
         $f = $this->request->getGet()->get('f');
@@ -31,11 +35,13 @@ class UserController extends MainController
             }else{
                 $form = $this->request->getPost()->get('form');
             }
-
             if (!empty($form)) {
-                if ($this->request->getPost()->get('token') === $this->request->getSession()->get('token')) {
-                    $message = $this->userEdit->edit($f, $form);
+                if (password_verify($this->request->getPost()->get('password'), $user->getPassword())) {
+                    if ($this->request->getPost()->get('token') === $this->request->getSession()->get('token')) {
+                        $message = $this->userEdit->edit($f, $form, $user);
+                    }
                 }
+                $message = 'Mauvais mot de passe';
             }
         }
         return $this->render('user/edit.html.twig', ['f' => $f, 'message' => $message]);
