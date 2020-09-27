300ce234d2168ffa6c915738a918357bb46ea6f6
granger kenolane <kenolane28@gmail.com>
2020-09-27 22:04:36 +0200

    fix the header and add recoverPasswordMethod

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -49,8 +49,10 @@ class SecurityController extends MainController
     {
         $user = $this->request->getSession()->get('user');
         $subject = 'Validation de votre email';
-        $message = 'Bonjour veuillez cliquez sur ce lien pour  valider votre email http://localhost:8000/index.php?access=security!verified&k='. $user->getRandomKey();
+        $message = 'Bonjour veuillez cliquez sur ce lien pour  valider votre email http://kenolane-granger.com/index.php?access=security!verified&k=' . $user->getRandomKey();
         mail($user->getEmail(),$subject,$message);
+        $this->request->getSession()->set('message', 'Le mail a bien été envoyer');
+        header("Location:index.php");
     }
 
     public function verifiedMethod()
@@ -69,14 +71,51 @@ class SecurityController extends MainController
     {
         if (!empty($this->request->getPost()->get('name')) && !empty($this->request->getPost()->get('email')) && !empty($this->request->getPost()->get('subject')) && !empty($this->request->getPost()->get('firstName')) && !empty($this->request->getPost()->get('content')))
         {
-            dump('test');
            $sender = $this->request->getPost()->get('email');
            $addressee = 'kenolane28@gmail.com';
            $subject = $this->request->getPost()->get('subject');
            $header = "From:". $sender;
            $content = $this->request->getPost()->get('content');
            mail($addressee,$subject,$content,$header);
-           header('Location:index.php');
+            header("Location:index.php");
         }
     }
+
+    public function recoverPasswordMethod()
+    {
+        $submit = $this->request->getPost()->get('submit');
+        if (isset($submit) && !empty($this->request->getPost()->get('email'))) {
+            $email = $this->security->verifiedEmail($this->request->getPost()->get('email'));
+            if (!empty($email)) {
+                $user = $this->em->getRepository(':User')->findOneBy(array('email' => $this->request->getPost()->get('email')));
+                $addressee = $user->getEmail();
+                $subject = 'Changer votre Mot de passe';
+                $content = 'Pour changer votre mot de passe cliquez sur ce lien http://kenolane-granger.com/index.php?access=security!changePassword&k=' . $user->getRandomKey() . '&e=' . $user->getEmail();
+                mail($addressee, $subject, $content);
+                header("Location:index.php");
+            }
+
+        }
+        return $this->render('security/recoverPassword.html.twig');
+    }
+
+    public function changePasswordMethod()
+    {
+        $user = $this->em->getRepository(':User')->findOneBy(array('email' => $this->request->getGet()->get('e')));
+        if ($this->request->getGet()->get('k') === $user->getRandomKey()) {
+            $submit = $this->request->getPost()->get('submit');
+            if (isset($submit) && !empty($this->request->getPost()->get('password1')) && !empty($this->request->getPost()->get('password2'))) {
+                if ($this->request->getPost()->get('password1') === $this->request->getPost()->get('password2')) {
+                    if (preg_match('/^(?=.*\d)(?=.*[-.*&^%$#@()/_])(?=.*[a-z])(?=.*[A-Z])[0-9A-Za-z@#\-_$%^&+=§!\?]{8,20}$/', $this->request->getPost()->get('password1'))) {
+                        $user->setPassword(password_hash($this->request->getPost()->get('password1'), PASSWORD_DEFAULT));
+                        $this->em->persist($user);
+                        $this->em->flush();
+                        header("Location:index.php");
+                    }
+                }
+            }
+            return $this->render('security/changePassword.html.twig');
+        }
+    }
+
 }
