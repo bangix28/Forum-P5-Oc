dfb5e194710ec30ed1042ee1a118e9551680b790
granger kenolane <kenolane28@gmail.com>
2020-10-04 21:55:55 +0200

    remove some things

--- src/Services/captcha/Captcha.php
+++ src/Services/captcha/Captcha.php
@@ -18,7 +18,7 @@ class Captcha extends MainController
         $response = $this->request->getPost()->get('recaptcha_response');
         if (isset($response)) {
             $recaptcha_url = 'https://www.google.com/recaptcha/api/siteverify';
-            $recaptcha_secret = CAPTCHA_KEY;
+            $recaptcha_secret = 'Votre clef coté serveur';
             $recaptcha_response = $this->request->getPost()->get('recaptcha_response');
 
             // Make and decode POST request:
